6fbdaf616a6d86d5e166afaabebc4a84133bfd3d
gurvinderji <gurvinderji2000@gmail.com>
2025-11-07 08:14:55 +0530

    regiser controller developed

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const userRoter = Router();
-
+userRoter.post(
+  "/register",
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 export default userRoter;
